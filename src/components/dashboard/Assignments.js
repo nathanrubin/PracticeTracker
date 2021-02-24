e6d6350869f0f3ec7bd2b2119890cab3b235db6d
Nathan Rubin <nathanr1@itrunwells-MacBook-Pro.local>
2021-02-24 13:23:29 -0800

    Updated with an ios force reload and assignment styles.

--- src/components/dashboard/Assignments.js
+++ src/components/dashboard/Assignments.js
@@ -7,6 +7,18 @@ import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Assignment from '@material-ui/icons/Assignment'
 import CardActionArea from '@material-ui/core/CardActionArea'
+import { DataGrid } from '@material-ui/data-grid';
+
+
+const columns = [
+  { field: 'assignment', headerName: 'Assignments', flex: 1 },
+];
+
+const rows = [
+  { id: 1, assignment: 'Ocean Waves – m.1-12 – 5x' },
+  { id: 2, assignment: '(Bingos Dance) 2x' },
+  { id: 3, assignment: 'Cossack Ride – Fast! 1x' },
+];
 
 function getAssignments() {
     return ["Ocean Waves – m.1-12 – 5x ", "(Bingos Dance) 2x ", "Cossack Ride – Fast! 1x "]
@@ -33,18 +45,9 @@ export default function Assignments() {
   const classes = useStyles();
   return (
     <React.Fragment>
-        {assignments.map( (assignment, i)  => 
-            <Grid key={i} item xs>
-                <CardActionArea component="a" href="#">
-                    <Paper button key={i} color="primary" className={classes.paper}>
-                        <Assignment color="primary"  />
-                        <Typography kkey={i} color="textSecondary">
-                            {assignment}
-                        </Typography>
-                    </Paper>
-                </CardActionArea>
-            </Grid>
-        )}
+      <Grid item xs>
+        <DataGrid rows={rows} columns={columns} rowHeight={28} hideFooter autoHeight checkboxSelection density disableColumnSelector={false}/>
+      </Grid>
     </React.Fragment>
   );
 }
